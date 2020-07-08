17d5bb54ab167cb3df7937ead83372c3a3f6055e
Raul Palacios <palacios.raul02@gmail.com>
2020-07-07 23:15:17 -0500

    callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+function soyUnCallback(data){
+    console.log('soy el callback, ahora me toca a mi')
+    console.log(data)
+}
+
+function haciendoUnaPeticionHTTP(callback){
+    console.log('Estoy realizando otras operaciones')
+    console.log('Ya casi termino....')
+
+    callback({
+        id: 1,
+        username: 'raul_palacios',
+        age: 25
+    })
+}
+
+haciendoUnaPeticionHTTP(soyUnCallback)
